0c898b50c0ed178ed7b25805780b9f63532c3926
Kim Seungha <seungha.me@gmail.com>
2018-01-18 16:28:10 +0900

    style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,25 @@
 import React, {Component} from 'react';
 import * as firebase from 'firebase';
+import styled from 'styled-components';
+
+const Wrap = styled.nav`
+  background-color: skyblue;
+  padding: 1em;
+  display: flex;
+  align-items: center;
+`;
+
+const InnerLeft = styled.div`
+  flex-grow: 1;
+  color: white;
+  font-size: 2em;
+`;
+
+const LogOutButton = styled.button`
+  padding: 1em;
+  border: none;
+  background-color: white;
+`;
 
 export default class NavBar extends Component {
   handleLogoutClick = () => {
@@ -7,9 +27,10 @@ export default class NavBar extends Component {
   }
   render() {
     return (
-      <div>
-        <button onClick={this.handleLogoutClick}>로그아웃</button>
-      </div>
+      <Wrap>
+        <InnerLeft>BBS</InnerLeft>
+        <LogOutButton onClick={this.handleLogoutClick}>로그아웃</LogOutButton>
+      </Wrap>
     )
   }
 }
